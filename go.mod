c2530fbfe2706346783f8fe47f8dc4b8e6c3ef76
Pranay Pandit <pranay@Pranays-MacBook-Air.local>
2026-06-18 17:07:13 +0530

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pranay/go-s3-test
+
+go 1.22
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
+
+func main() {
+	fmt.Printf("Hello from Go %s on %s/%s\n", runtime.Version(), runtime.GOOS, runtime.GOARCH)
+	fmt.Printf("Executable: %s\n", os.Args[0])
+}
