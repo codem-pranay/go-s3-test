0e9f6645059314858b4f6d858f0cd65ac94554e5
Pranay Pandit <pranay@Pranays-MacBook-Air.local>
2026-06-18 18:19:53 +0530

    Updated yml

--- main.go
+++ main.go
@@ -7,6 +7,6 @@ import (
 )
 
 func main() {
-	fmt.Printf("Hello, Go %s on %s/%s\n", runtime.Version(), runtime.GOOS, runtime.GOARCH)
+	fmt.Printf("Hello, Go V1 %s on %s/%s\n", runtime.Version(), runtime.GOOS, runtime.GOARCH)
 	fmt.Printf("Executable: %s\n", os.Args[0])
 }
